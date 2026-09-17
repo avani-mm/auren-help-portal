6ff4ee23e1ac59d3d8dd2d4f6d6668d3e3f9e67c
avani-mm <avanimanoj63@gmail.com>
2026-09-17 23:18:37 +0530

    Serve React frontend from Express

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("dist"));
 app.use((req, res, next) => {
   console.log("REQUEST:", req.method, req.url);
   next();
@@ -138,6 +139,9 @@ ${dateTime}
       error: "Unable to send the email notification.",
     });
   }
+});
+app.get("*", (req, res) => {
+  res.sendFile("index.html", { root: "dist" });
 });
 
 const PORT = process.env.PORT || 3001;
